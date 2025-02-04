66ef67c98c34bef687a8e49b73628ac09cd7d011
Honsage <musatov.i.a@edu.mirea.ru>
2025-02-04 19:53:59 +0300

    feat(rational): add unary and binary operations of rational numbers

--- Math.h
+++ Math.h
@@ -2,6 +2,7 @@
 #define MATH_H
 
 #include <fstream>
+#include <iostream>
 #include <utility>
 class Rational;
 
@@ -15,12 +16,20 @@ class Rational;
 #define Rational Rational
 #define Real double
 
+#define INF INT_MAX
+
 #endif
 
 namespace Math {
     inline Real abs(const Real& a) {
         if (a < 0) return -1 * a;
         return a;
+    }
+
+    inline Real sign(const Real& a) {
+        if (a < 0) return -1;
+        if (a == 0) return 0;
+        return 1;
     }
 
     inline Integer gcd(const Integer& a, const Integer& b) {
@@ -51,12 +60,84 @@ public:
         this->denominator = rat.denominator;
     }
 
+    void reduce() {
+        const Integer gcd = Math::gcd(this->numerator, static_cast<Integer>(this->denominator));
+        this->numerator /= gcd;
+        this->denominator /= gcd;
+    }
+
+    void reverse() {
+        Integer new_numerator;
+        Natural new_denominator;
+        switch (Math::sign(this->numerator)) {
+            case -1: {
+                new_numerator = -1 * static_cast<Integer>(this->denominator);
+                new_denominator = static_cast<Natural>(Math::abs(this->numerator));
+                break;
+            }
+            case 0: {
+                std::cerr << "Division by zero!";
+                return;
+            }
+            case 1: {
+                new_numerator = static_cast<Integer>(this->denominator);
+                new_denominator = static_cast<Natural>(Math::abs(this->numerator));
+                break;
+            }
+            default: {
+                return;
+            }
+        }
+        this->numerator = new_numerator;
+        this->denominator = new_denominator;
+    }
+
+    [[nodiscard]]
+    Real cast() const {
+        return static_cast<Real>(this->numerator) / this->denominator;
+    }
+
     Rational& operator= (const Rational& rat) = default;
 
     Rational& operator= (const std::pair<Integer, Natural>& pair) {
         this->numerator = pair.first;
         this->denominator = pair.second;
         return *this;
+    }
+
+    friend Rational operator* (const Rational& lhs, const Rational& rhs) {
+        Rational res(lhs.numerator * rhs.numerator, lhs.denominator * rhs.denominator);
+        res.reduce();
+        return res;
+    }
+
+    friend Rational operator/ (const Rational& lhs, Rational rhs) {
+        try {
+            rhs.reverse();
+        } catch (...) {
+            return Rational(INF, 1);
+        }
+        Rational res = lhs * rhs;
+        res.reduce();
+        return res;
+    }
+
+    friend Rational operator+ (const Rational& lhs, const Rational& rhs) {
+        const Natural common_denominator = Math::lcm(static_cast<Integer>(lhs.denominator), static_cast<Integer>(rhs.denominator));
+        const Integer lhs_numerator = static_cast<Integer>(common_denominator / lhs.denominator) * lhs.numerator;
+        const Integer rhs_numerator = static_cast<Integer>(common_denominator / rhs.denominator) * rhs.numerator;
+        Rational res(lhs_numerator + rhs_numerator, common_denominator);
+        res.reduce();
+        return res;
+    }
+
+    friend Rational operator- (const Rational& lhs, const Rational& rhs) {
+        const Natural common_denominator = Math::lcm(static_cast<Integer>(lhs.denominator), static_cast<Integer>(rhs.denominator));
+        const Integer lhs_numerator = static_cast<Integer>(common_denominator / lhs.denominator) * lhs.numerator;
+        const Integer rhs_numerator = static_cast<Integer>(common_denominator / rhs.denominator) * rhs.numerator;
+        Rational res(lhs_numerator - rhs_numerator, common_denominator);
+        res.reduce();
+        return res;
     }
 
     friend std::ostream& operator<<(std::ostream& os, const Rational& rat) {
